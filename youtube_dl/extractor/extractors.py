51e8a149b01c31588437dc7875b75171c75657fe
Remita Amine <remitamine@gmail.com>
2017-12-23 13:21:33 +0100

    [7plus] Add new extractor(closes #15043)

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -927,6 +927,7 @@ from .senateisvp import SenateISVPIE
 from .sendtonews import SendtoNewsIE
 from .servingsys import ServingSysIE
 from .servus import ServusIE
+from .sevenplus import SevenPlusIE
 from .sexu import SexuIE
 from .shahid import (
     ShahidIE,
