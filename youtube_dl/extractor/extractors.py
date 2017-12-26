782cfa09f1b836f5324d5847220295477e2f9dc3
Remita Amine <remitamine@gmail.com>
2017-12-26 12:32:04 +0100

    [umg:de] Add new extractor(closes #11582)(closes #11584)

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -1144,6 +1144,7 @@ from .udemy import (
 from .udn import UDNEmbedIE
 from .uktvplay import UKTVPlayIE
 from .digiteka import DigitekaIE
+from .umg import UMGDeIE
 from .unistra import UnistraIE
 from .unity import UnityIE
 from .uol import UOLIE
